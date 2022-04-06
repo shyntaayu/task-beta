0e77e4ec0812bda4de4c6a7599534656881f3f0b
shyntaayu <shynta.add@gmail.com>
2022-04-06 18:48:25 +0700

    + condition rendering

---
+++ pages/condition.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <h1 v-if="num == 0">Angkanya 0</h1>
+    <h1 v-else-if="num > 0">Angkanya positif</h1>
+    <h1 v-else-if="num < 0">Angkanya negatif</h1>
+    <h1 v-else>Bukan angka 0</h1>
+
+    <h1>Title</h1>
+    <p>Paragraph 1</p>
+    <p>Paragraph 2</p>
+    <div v-if="ok">
+      <label>Username</label>
+      <input placeholder="Enter your username" key="username-input" />
+    </div>
+    <div v-else>
+      <label>Email</label>
+      <input placeholder="Enter your email address" key="username-input" />
+    </div>
+    <button @click="ok = !ok">Toggle OK</button>
+    <h1 v-show="ok">Ini Pake V-SHow</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      num: "-4 aa",
+      ok: false,
+    };
+  },
+};
+</script>
